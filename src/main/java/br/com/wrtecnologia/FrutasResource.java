2539f2f46c049d3ff206819310ae2e59778bfaaa
Wagner Pires <wagnerdba@gmail.com>
2024-12-17 21:16:00 -0300

    Tests

---
+++ src/main/java/br/com/wrtecnologia/FrutasResource.java
@@ -0,0 +1,39 @@
+package br.com.wrtecnologia;
+
+import jakarta.transaction.Transactional;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+import java.util.List;
+
+@Path("/frutas")
+public class FrutasResource {
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Fruta> list() {
+        return Fruta.listAll();
+    }
+
+    @POST
+    //@Produces(MediaType.APPLICATION_JSON)
+    @Transactional
+    public void newFruta() {
+        Fruta fruta = new Fruta();
+        fruta.nome = "Laranja";
+        fruta.qtd = 5;
+        fruta.persist();
+    }
+}
+
+// mvn quarkus:list-extensions
+
+// mvn quarkus:add-extensions -Dextensions="quarkus-hibernate-orm-panache"
+// mvn quarkus:add-extensions -Dextensions="quarkus-jdbc-postgresql"
+
+
+
+
